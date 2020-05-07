8615b8f0b0adc3195135f273a9049ab8c425f608
Lihini Nisansala <lihininisansala95@gmail.com>
2020-05-08 03:01:40 +0530

    delete algo.py

--- algo.py
+++
@@ -1,9 +0,0 @@
-residents= []
-houses = int(input("Number of houses:"))
-
-for i in range(houses):
-    residents_per_house= int(input("no of residents:"))
-    residents.append(residents_per_house)  
-print("total residents:",sum(residents))
-
-
